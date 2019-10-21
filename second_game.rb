503d6c75cee1eb5fbe0cb4c378dfaf0d22549d66
James <jamessaunders@Hungs-Air.Talentful>
2019-10-21 11:52:01 +0100

    Checking on second repository

--- second_game.rb
+++ second_game.rb
@@ -3,9 +3,9 @@ random_number = rand(10) + 1
 puts "Pick any number between 1 & 10:"
 guess = gets.chomp.to_i
 
-until guess == random_number      
+until guess == random_number
         puts "Bad luck, try again!"
         guess = gets.chomp.to_i
 end
 
-puts "Woo that's correct!"
+puts "Woo that's correct! Well done"
